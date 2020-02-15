7f747e90d898f033ab05e3f18c3852f6c1052c4b
rickbala <ricardovbalarini@gmail.com>
2020-02-15 11:11:12 -0300

    user.name

---
+++ src/main/java/oca/LearningMethods.java
@@ -0,0 +1,18 @@
+package oca;
+
+public class LearningMethods {
+
+	public static void main(String[] args) {
+
+//		System.out.println(System.getProperties());
+//		System.out.println(System.getProperty("os.name"));
+//		System.out.println(System.getProperty("user.name"));
+//		System.out.println(System.getProperty("java.vm.specification.version"));
+//		System.out.println(System.getProperty("user.home"));
+
+		System.out.println();
+		System.out.println("Hello, " + System.getProperty("user.name") + "!");
+
+	}
+
+}
