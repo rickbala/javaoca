398d048bf71cb59e43563ae1194c9f9ec401519a
rickbala <ricardovbalarini@gmail.com>
2020-03-04 20:51:44 -0300

    looping

---
+++ src/main/java/oca/LoopTests.java
@@ -0,0 +1,10 @@
+package oca;
+
+public class LoopTests {
+
+	public static void main(String[] args) {
+		for (int i = 0, j= 10; i<10 && j<20 ; i++, j++){
+			System.out.println(i + " " +  j);
+		}
+	}
+}
